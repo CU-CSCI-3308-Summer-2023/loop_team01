d436e8fa067712d4e818472e5b483e7f73d41d40
jeremySchur <jeremy.schur02@gmail.com>
2023-07-03 20:44:12 -0600

    fixed some file formatting

---
+++ All_Project_Code/components/src/index.js
@@ -0,0 +1,30 @@
+// importing the dependencies
+// Express is a NodeJS framework that, among other features, allows us to create HTML templates.
+const express = require('express');
+const bodyParser = require('body-parser');
+const pgp = require('pg-promise')();
+require('dotenv').config();
+
+// defining the Express app
+const app = express();
+// View engine setup
+app.set('view engine', 'ejs');
+// using bodyParser to parse JSON in the request body into JS objects
+app.use(bodyParser.json());
+// Database connection details
+const dbConfig = {
+  host: process.env.POSTGRES_HOST,
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  ssl: true
+};
+// Connect to database using the above details
+const db = pgp(dbConfig);
+
+// <!-- Endpoint 1 :  Default endpoint ("/") -->
+const message = 'Hey there!';
+app.get('/', (req, res) => {
+  res.send(message);
+});
