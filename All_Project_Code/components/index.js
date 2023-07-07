83e2c12b51516cdcfc7c9dbf4a772dba786d331c
jeremySchur <jeremy.schur02@gmail.com>
2023-07-07 14:30:51 -0600

    server now works with ejs

--- All_Project_Code/components/index.js
+++ All_Project_Code/components/index.js
@@ -3,17 +3,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pgp = require('pg-promise')();
+const path = require("path");
 require('dotenv').config();
 const session = require("express-session");
 
 // defining the Express app
 const app = express();
+const staticDir = path.join(__dirname, "resources");
+app.use(express.static(staticDir));
 // using bodyParser to parse JSON in the request body into JS objects
 app.use(bodyParser.json());
 // Database connection details
 const dbConfig = {
   host: process.env.POSTGRES_HOST,
-  port: 4000,
+  port: 5432,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
@@ -35,7 +38,6 @@ db.connect()
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
-app.use(bodyParser.json());
 
 // set session
 app.use(
@@ -53,9 +55,8 @@ app.use(
 );
 
 // <!-- Endpoint 1 :  Default endpoint ("/") -->
-const message = 'Hey there!';
 app.get('/', (req, res) => {
-  res.send(message);
+  res.render("pages/home");
 });
 
 
