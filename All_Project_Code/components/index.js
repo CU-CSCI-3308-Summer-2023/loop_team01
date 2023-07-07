9ae195ec617489eb4886de3960ff5a9671788b85
jeremySchur <jeremy.schur02@gmail.com>
2023-07-06 23:10:56 -0600

    works with ejs now

--- All_Project_Code/components/index.js
+++ All_Project_Code/components/index.js
@@ -1,12 +1,10 @@
 // importing the dependencies
 // Express is a NodeJS framework that, among other features, allows us to create HTML templates.
-
-console.log('Starting up...');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const pgp = require('pg-promise')();
 require('dotenv').config();
+const session = require("express-session");
 
 // defining the Express app
 const app = express();
@@ -15,7 +13,7 @@ app.use(bodyParser.json());
 // Database connection details
 const dbConfig = {
   host: process.env.POSTGRES_HOST,
-  port: 5432,
+  port: 4000,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
@@ -24,12 +22,47 @@ const dbConfig = {
 // Connect to database using the above details
 const db = pgp(dbConfig);
 
+// db test
+db.connect()
+  .then((obj) => {
+    // Can check the server version here (pg-promise v10.1.0+):
+    console.log("Database connection successful");
+    obj.done(); // success, release the connection;
+  })
+  .catch((error) => {
+    console.log("ERROR:", error.message || error);
+  });
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+
+// set session
+app.use(
+  session({
+    secret: "XASDASDA",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
 // <!-- Endpoint 1 :  Default endpoint ("/") -->
 const message = 'Hey there!';
 app.get('/', (req, res) => {
   res.send(message);
 });
 
+
+
+
+
+// Listening on port 4000
 app.listen(4000, () => {
   console.log('listening on port 4000');
 });
